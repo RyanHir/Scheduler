d5c4adffa23d6c10f75ffac1622ec25fb4291992
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-11 17:06:17 -0500

    Fixed url so google play can open it

--- src/client/js/Initialization.js
+++ src/client/js/Initialization.js
@@ -178,7 +178,7 @@ function viewGradeSelect(){
 	html += "<div style='margin-top: 2em;'>";
 	html += "Mobile Applicaions";
 	html += "<br>";
-	html += "<a href='https://play.google.com/store/apps/details?id=com.ryanhirasaki.scheduler'>";
+	html += "<a href='https://play.google.com/store/apps/details?id=com.ryanhirasaki.scheduler&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>";
 	html += "<img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png' style='width: 14em;'>";
 	html += "</a>";
 	html += "<br>";
